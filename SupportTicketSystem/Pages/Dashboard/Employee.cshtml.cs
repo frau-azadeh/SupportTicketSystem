a345594660b371089a023f9fcc0d9c85b753f53c
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-19 22:06:03 +0330

    add: config route dashboard page

---
+++ SupportTicketSystem/Pages/Dashboard/Employee.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SupportTicketSystem.Pages.Dashboard
+{
+    public class EmployeeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
