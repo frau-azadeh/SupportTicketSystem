6d9999b6813d5a9ec1bebc7dde90d75d92e2a441
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-19 11:32:09 +0330

    Add project files.

---
+++ SupportTicketSystem/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./Pages/**/*.cshtml", "./Views/**/*.cshtml", "./wwwroot/js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
