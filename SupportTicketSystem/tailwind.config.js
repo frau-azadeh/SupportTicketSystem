9985d90598ebfe321540c36ef13e74096b4e356c
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-19 21:03:49 +0330

    update: config animation

--- SupportTicketSystem/tailwind.config.js
+++ SupportTicketSystem/tailwind.config.js
@@ -6,8 +6,16 @@ module.exports = {
             fontFamily: {
                 vazir: ['Vazir', 'sans-serif'],
             },
+            animation: {
+                'fade-in': 'fadeIn 0.5s ease-out',
+            },
+            keyframes: {
+                fadeIn: {
+                    '0%': { opacity: '0', transform: 'scale(0.95)' },
+                    '100%': { opacity: '1', transform: 'scale(1)' },
+                },
+            },
         },
-    },
-    plugins: [],
+        plugins: [],
+    }
 }
-
