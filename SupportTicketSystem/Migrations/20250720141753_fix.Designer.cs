d99ab45ce34b716b3af3dd32bf490c3cb63e84f9
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-20 17:52:41 +0330

    migration for database table change

--- SupportTicketSystem/Migrations/20250720141753_fix.Designer.cs
+++ SupportTicketSystem/Migrations/20250720141753_fix.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SupportTicketSystem.Data;
 
@@ -11,9 +12,11 @@ using SupportTicketSystem.Data;
 namespace SupportTicketSystem.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250720141753_fix")]
+    partial class fix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
