d82d982de94c95e83c486c26c0f1bbc382719601
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-20 17:54:16 +0330

    update: set value for Title, Description, Priority

--- SupportTicketSystem/Models/Ticket.cs
+++ SupportTicketSystem/Models/Ticket.cs
@@ -5,12 +5,12 @@ namespace SupportTicketSystem.Models
     public class Ticket
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; } 
+        public string Title { get; set; } = null;
+        public string Description { get; set; } = null;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public string Status { get; set; } = "در انتظار بررسی";
 
-        public string? Priority { get; set; }    
+        public string? Priority { get; set; } = null;   
         public string? AttachmentPath { get; set; }  
 
 
