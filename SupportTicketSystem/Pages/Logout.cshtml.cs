ee30a02c66d32fe9c7f40010be4a3bcee9f957a3
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-21 19:24:07 +0330

    add: logout page

---
+++ SupportTicketSystem/Pages/Logout.cshtml.cs
@@ -0,0 +1,16 @@
+﻿// Pages/Dashboard/Logout.cshtml.cs
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SupportTicketSystem.Pages.Dashboard
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnGet()
+        {
+            await HttpContext.SignOutAsync(); // لاگ‌اوت از کوکی
+            return RedirectToPage("/Login");  // هدایت به صفحه لاگین
+        }
+    }
+}
