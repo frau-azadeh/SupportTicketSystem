048047d92f153e46ef0663e78026d83c7db7bc9c
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-21 18:52:42 +0330

    update: add bool for isRead notification

--- SupportTicketSystem/Models/Notification.cs
+++ SupportTicketSystem/Models/Notification.cs
@@ -12,8 +12,9 @@ namespace SupportTicketSystem.Models
         public string Message { get; set; } = string.Empty;
         public string SenderName { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
+        public bool IsRead { get; set; } = false;
 
-     
+
         [ForeignKey("UserId")]
         public User? User { get; set; }
     }
