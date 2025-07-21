f77c653138fb93ac9a4a239f8f73559a805e56eb
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-21 18:00:10 +0330

    migration database for notification

---
+++ SupportTicketSystem/Migrations/20250721141041_fix5.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SupportTicketSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRead",
+                table: "Notifications");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SenderName",
+                table: "Notifications",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TicketId",
+                table: "Notifications",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SenderName",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "TicketId",
+                table: "Notifications");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRead",
+                table: "Notifications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
