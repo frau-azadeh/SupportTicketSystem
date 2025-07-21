bd6a65eddbd0e6526a61077533eedbfc029778df
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-21 18:01:42 +0330

    add: Notification Models

---
+++ SupportTicketSystem/Models/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupportTicketSystem.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public int UserId { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+        public string SenderName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+
+        // 👇 این خط جدید رو اضافه کن
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
+    }
+}
